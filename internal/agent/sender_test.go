e480ee505de026ad39fa4c0d425b241288556b4c
Dima <goodd214080@gmail.com>
2025-06-27 17:17:02 +0300

    feat: added tests for sender

--- internal/agent/sender_test.go
+++ internal/agent/sender_test.go
@@ -1,25 +1,69 @@
 package agent
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	models "github.com/makimaki04/go-metrics-agent.git/internal/model"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
+type mockStorage struct{}
+
+func (m *mockStorage) GetAll() map[string]models.Metrics {
+	val := 123.45
+	delta := int64(10)
+
+	return map[string]models.Metrics{
+		"test_gauge": {
+			ID:    "test_gauge",
+			MType: "gauge",
+			Value: &val,
+		},
+		"test_counter": {
+			ID:    "test_counter",
+			MType: "counter",
+			Delta: &delta,
+		},
+	}
+}
+
 func TestSender_SendMetrics(t *testing.T) {
-	type args struct {
-		metrics map[string]models.Metrics
+	var receivedRequests []struct {
+		Method  string
+		Path    string
+		Headers http.Header
 	}
-	tests := []struct {
-		name string
-		s    Sender
-		args args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.s.SendMetrics()
-		})
+
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		receivedRequests = append(receivedRequests, struct{
+			Method string
+			Path string
+			Headers http.Header
+			}{
+				Method: r.Method,
+				Path: r.URL.Path,
+				Headers: r.Header.Clone(),
+			})
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "text/plain")
+	}))
+
+	defer testServer.Close()
+
+	storage := &mockStorage{}
+	sender := NewSender(testServer.Client(), testServer.URL, storage)
+
+	sender.SendMetrics()
+
+	require.Len(t, receivedRequests, 2)
+
+	for _, req := range receivedRequests {
+		assert.Equal(t, http.MethodPost, req.Method)
+		assert.True(t, strings.HasPrefix(req.Path, "/update/"))
+		assert.Equal(t, "text/plain", req.Headers.Get("Content-Type"))
 	}
 }
