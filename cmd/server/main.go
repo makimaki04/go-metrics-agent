51dff9322e49f9c78ca5eb550f5c98e7337f63aa
Dima <goodd214080@gmail.com>
2025-07-27 15:13:05 +0300

    fix: fixed bugs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -28,38 +29,13 @@ func main() {
 	service := service.NewService(storage)
 	handler := handler.NewHandler(service)
 
-	if cfg.Restore {
-		file, error := os.OpenFile(cfg.FilePath, os.O_CREATE|os.O_RDWR, 0666)
-		if error != nil {
-			logger.Fatal("Couldn't create or open the local storage file")
-		}
-		defer file.Close()
-		data, err := file.Stat()
-		if err != nil {
-			logger.Info("Couldn't read the file")
-		}
-		
-		if data.Size() > 0 {
-			var  metrics struct {
-				Counters map[string]int64    `json:"counters"`
-				Gauges   map[string]float64  `json:"gauges"`
-			}
-			decoder := json.NewDecoder(file)
-			if err := decoder.Decode(&metrics); err != nil {
-				logger.Info("Couldn't parse data")
-			} else {
-				for key, value := range metrics.Gauges {
-					service.UpdateGauge(key, value)
-				}
+	dir := filepath.Dir(cfg.FilePath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		 logger.Fatal("Couldn't create directory for storage file", zap.Error(err))
+	}
 
-				for key, value := range metrics.Counters {
-					service.UpdateCounter(key, value)
-				}
-				logger.Info("metrics successfully loaded from local storage located in ./data/save.json")
-			}
-		} else {
-			logger.Info("the local file exists, but is empty")
-		}
+	if cfg.Restore {
+		loadMetricsFromFile(cfg.FilePath, service, logger)
 	}
 
 	r := chi.NewRouter()
@@ -78,7 +54,7 @@ func main() {
 			})
 		})
 		
-	})
+	})	
 
 	go func() {
 		err := http.ListenAndServe(cfg.Address, r)
@@ -88,24 +64,60 @@ func main() {
 	}()
 
 	if cfg.StoreInt == 0 {
-		saveMetrcisToFile(service, logger)
+		saveMetrcisToFile(cfg.FilePath, service, logger)
 	} else {
 		ticker := time.NewTicker(time.Duration(cfg.StoreInt) * time.Second)
 		defer ticker.Stop()
 
 		for range ticker.C {
-			saveMetrcisToFile(service, logger)
+			saveMetrcisToFile(cfg.FilePath, service, logger)
 		}
 	}
-
 }
 
-
-func saveMetrcisToFile(service service.MetricsService, logger *zap.Logger) {
-	file, error := os.OpenFile(cfg.FilePath, os.O_CREATE|os.O_RDWR, 0666)
-	if error != nil {
-		logger.Fatal("Couldn't create or open the local storage file")
+func loadMetricsFromFile(path string, service service.MetricsService, logger *zap.Logger) {
+	file, err := os.OpenFile(path, os.O_RDONLY|os.O_CREATE, 0666)
+	if err != nil {
+		logger.Error("Failed to open metrics file for reading", zap.Error(err))
+		return
 	}
+	defer file.Close()
+
+	data, err := file.Stat()
+	if err != nil {
+		logger.Info("Couldn't read the file")
+	}
+	
+	if data.Size() > 0 {
+		var  metrics struct {
+			Counters map[string]int64    `json:"counters"`
+			Gauges   map[string]float64  `json:"gauges"`
+		}
+		decoder := json.NewDecoder(file)
+		if err := decoder.Decode(&metrics); err != nil {
+			logger.Info("Couldn't parse data")
+		} else {
+			for key, value := range metrics.Gauges {
+				service.UpdateGauge(key, value)
+			}
+
+			for key, value := range metrics.Counters {
+				service.UpdateCounter(key, value)
+			}
+			logger.Info("metrics successfully loaded from local storage located in ./data/save.json")
+		}
+	} else {
+		logger.Info("the local file exists, but is empty")
+	}
+}
+
+func saveMetrcisToFile(path string, service service.MetricsService, logger *zap.Logger) {
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+	if err != nil {
+		logger.Error("Failed to open metrics file for writing", zap.Error(err))
+		return
+	}
+	defer file.Close()
 
 	allMetrics := struct {
 			Counters map[string]int64    `json:"counters"`
@@ -118,16 +130,6 @@ func saveMetrcisToFile(service service.MetricsService, logger *zap.Logger) {
 		data, err := json.MarshalIndent(allMetrics, "", "	")
 		if err != nil {
 			logger.Error("Failed to marshal all metrics", zap.Error(err))
-			return
-		}
-
-		if err := file.Truncate(0); err != nil {
-			logger.Error("Failed to truncate file", zap.Error(err))
-			return
-		}
-		
-		if _, err := file.Seek(0, 0); err != nil {
-			logger.Error("Failed to seek to beginning of file", zap.Error(err))
 			return
 		}
 
